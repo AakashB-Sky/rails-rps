7f6c06d29a4f724c8e9850bd4a2e1c6b1c96712f
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-06 00:11:36 +0000

    rock Ruby action written, messing with HTML layout

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,5 +1,18 @@
+moves = ["rock", "paper", "scissors"]
+
 class MovesController < ApplicationController  
+  
   def rock
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome == "tied"
+    elsif @comp_move == "paper"
+      @outcome == "lost"
+    else @comp_move == "scissors"
+      @outcome == "won"
+    end
+    
     render({ :template => "game_templates/play_rock" })
   end
 
