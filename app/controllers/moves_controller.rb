fd6ed41cd4abf490f037445a441655d0233f245c
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-06 00:23:00 +0000

    ostensibly done

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -15,10 +15,32 @@ class MovesController < ApplicationController
   end
 
   def paper
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
     render ({ :template => "game_templates/play_paper"})
   end
 
   def scissors
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
     render ({ :template => "game_templates/play_scissors"})
   end
 end
